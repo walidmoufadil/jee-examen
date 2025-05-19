7eb109b7565664d432601e1905545decde7458cc
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-05-19 07:51:26 +0000

    first commit

---
+++ src/main/java/com/ensetsdia/jeeexamen/JeeExamenApplication.java
@@ -0,0 +1,13 @@
+package com.ensetsdia.jeeexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeExamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeExamenApplication.class, args);
+    }
+
+}
